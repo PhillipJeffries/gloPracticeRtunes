12d7ba9b0f292c2d4bd01127c3acd25ba1e3b9d7
Dmitriy <ggbox@mail.ru>
2020-12-22 18:50:56 +0400

    1
    first commit

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+import {radioPlayerInit} from './radioPlayer.js';
+import {audioPlayerInit} from './audioPlayer.js';
+import {videoPlayerInit} from './videoPlayer.js';
+
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+//функция перебирает коллекцию элементов
+const deactivationPlayer = () => {
+  temp.style.display = 'none';
+  playerBtn.forEach((item) => item.classList.remove('active')); //фигурные скобки можно убрать, если выржение помещается на одной строке
+  playerBlock.forEach(item => item.classList.remove('active')); // если передаётся только один аргумент, то можно убрать круглые скобки
+};
+
+playerBtn.forEach((btn, i) => {
+  btn.addEventListener('click', ()=> {
+    deactivationPlayer();
+    btn.classList.add('active');
+    playerBlock[i].classList.add('active')
+  })
+})
+
+
+radioPlayerInit();
+audioPlayerInit();
+videoPlayerInit();
